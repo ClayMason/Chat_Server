509884d8ae88f443e23478adf5cae30205843cbe
Abdul-Muiz Yusuff <muiz.project@gmail.com>
2019-08-12 01:04:00 -0700

    Implemented LOGOUT and SEND TCP protocol functions

--- server.c
+++ server.c
@@ -29,13 +29,14 @@ pthread_mutex_t user_db_mutex = PTHREAD_MUTEX_INITIALIZER;
 char* udp_cmd_lst[2];
 char* tcp_cmd_lst[5];
 
-char ** snag (char* message, int* size);
+char ** snag (char* message, int* size, int size_limit);
 short command_index (char* message, char** command_list, int cmd_size);
 void * tcp_client_enter (void* args);
 int isAlnumString (char* str);
 int contains (char** lst, int size, char* wrd);
 void logout (int user_index);
 int find(char** lst, int size, char* wrd);
+void who (char** buffer, int* bytes);
 
 int main (int argc, char** argv) {
   // parse arguments
@@ -194,9 +195,11 @@ void * tcp_client_enter (void* args) {
   printf ("CHILD %lu: Connected to client (sd -> %d).\n", (unsigned long) pthread_self(), client_sd);
 
   char buffer[BUFFER_SIZE];
+  char* alloc_buffer = calloc (BUFFER_SIZE, sizeof(char));
   int n;
   int logged_in = 0; // false
   char* login_username;
+  int bytes_;
 
   while (1) {
     n = recv (client_sd, buffer, BUFFER_SIZE, 0);
@@ -226,6 +229,9 @@ void * tcp_client_enter (void* args) {
 
       else {
         // Application protocol handeling
+        #ifdef DEBUG
+        printf ("User Command: %s\n", tcp_cmd_lst[cmd_index]);
+        #endif
 
         // (1) LOGIN function
         if (strcmp(tcp_cmd_lst[cmd_index], "LOGIN") == 0) {
@@ -282,16 +288,9 @@ void * tcp_client_enter (void* args) {
         // (2) WHO function
         else if (strcmp(tcp_cmd_lst[cmd_index], "WHO") == 0) {
           // return all of the people on the server...
-          strcpy (buffer, "OK!\n");
-          int offset = 4;
-          for ( int i = 0; i < user_db_index; ++i ) {
-            strcpy (buffer+offset, user_database[i]);
-            offset += strlen(user_database[i]);
-            buffer[offset] = '\n';
-            ++offset;
-          }
-
-          send (client_sd, (void *) buffer, offset, 0);
+          printf ("About to check who()...\n");
+          who(&alloc_buffer, &bytes_);
+          send (client_sd, (void *) alloc_buffer, bytes_, 0);
         }
 
         // (3) LOGOUT function
@@ -302,8 +301,11 @@ void * tcp_client_enter (void* args) {
             send (client_sd, (void *) error_msg, 21, 0);
           }
           else {
+            pthread_mutex_lock(&user_db_mutex);
             int login_index = find(user_database, user_db_index, login_username);
+            printf ("Currently logged in as (%s)...\n", user_database[login_index]);
             logout (login_index);
+            pthread_mutex_unlock(&user_db_mutex);
 
             char success_msg[] = "OK!\n";
             send (client_sd, (void *) success_msg, 4, 0);
@@ -311,17 +313,96 @@ void * tcp_client_enter (void* args) {
         }
         // (4) SEND function
         else if (strcmp(tcp_cmd_lst[cmd_index], "SEND") == 0) {
+          // char ** snag (char* message, int* size, int size_limit)
+          int query_size;
+          char** query = snag(buffer+5, &query_size, 2);
 
+          #ifdef DEBUG
+          printf ("Size of Query: %d\n", query_size);
+          for ( int i = 0; i < query_size; ++i ) {
+            printf ("\t[%s:%ld]\n", *(query+i), strlen(*(query+i)));
+          }
+          #endif
+
+          int valid_request = 1;
+          int msg_len = atoi (*(query+1));
+          int recipient_index;
+          // check message validity
+          // int find(char** lst, int size, char* wrd)
+          if ( msg_len < 1 || msg_len > 990 ) {
+            char err_msg[] = "ERROR Invalid msglen\n";
+            send (client_sd, (void *) err_msg, 21, 0);
+            valid_request = 0;
+          }
+
+          if ( !logged_in ) {
+            char err_msg[] = "ERROR not logged in\n";
+            send (client_sd, (void *) err_msg, 20, 0);
+            valid_request = 0;
+          }
+
+          pthread_mutex_lock (&user_db_mutex);
+          recipient_index = find(user_database, user_db_index, *(query));
+          if ( recipient_index == -1 ) {
+            char err_msg[] = "ERROR Unknown userid\n";
+            send (client_sd, (void *) err_msg, 21, 0);
+            valid_request = 0;
+          }
+
+          if (valid_request) {
+            // send the ok to the sender first...
+            char success_msg[] = "OK!\n";
+            send (client_sd, (void *) success_msg, 4, 0);
+            // send the message to the recipient
+            #ifdef DEBUG
+            printf ("message unpacking sizes...:\n\t[FROM ] = 5\n");
+            printf ("\t[%s ] = %ld\n",login_username, strlen(login_username) + 1);
+            printf ("\t[%s ] = %ld\n",*(query+1), strlen(*(query+1)) + 1);
+          #endif
+            int amount = snprintf (buffer, BUFFER_SIZE, "FROM %s %d %s\n",
+              login_username, msg_len, buffer+5+strlen(login_username)+strlen(*(query+1))+2);
+            buffer[amount] = '\0';
+
+            send (user_fds[recipient_index], (void *) buffer, strlen(buffer), 0);
+
+          }
+          pthread_mutex_unlock (&user_db_mutex);
+
+          // free the query
+          for ( int i = 0; i < query_size; ++i )
+            free (*(query+i));
+          free (query);
         }
         // (5) BROADCAST function
         else if (strcmp(tcp_cmd_lst[cmd_index], "BROADCAST") == 0) {
+          int query_size;
+          char ** query;
 
+          query = snag(buffer+10, &query_size, 1);
+          #ifdef DEBUG
+          printf ("BROADCAST Query: (size -> %d)\n", query_size);
+          printf ("msglen: %s\n", *query);
+          #endif
+
+          pthread_mutex_lock(&user_db_mutex);
+          int broadcast_len = atoi(*query);
+          printf ("TEST: %s\n", buffer+10+strlen(*query)+1);
+          int amount = snprintf (buffer, BUFFER_SIZE, "FROM %s %d %s\n",
+            login_username, broadcast_len, buffer+10+strlen(*query)+1);
+            buffer[amount] = '\0';
+          for ( int i = 0; i < user_db_index; ++i ) {
+            // send to all
+            send (user_fds[i], (void *) buffer, strlen(buffer), 0);
+          }
+          pthread_mutex_unlock(&user_db_mutex);
         }
       }
 
     }
 
   }
+  // free
+  free (alloc_buffer);
 }
 
 
@@ -346,9 +427,10 @@ short command_index (char* message, char** command_list, int cmd_size) {
   return -1;
 }
 
-char ** snag (char* message, int* size) {
+char ** snag (char* message, int* size, int size_limit) {
   // given a message, snag the words one by one and return it
   // in a char* arr. The size of the char* arr is returned
+  // size_limit of 0 for no size limit
 
   // ASSUME: message must end with a '\0'
   char** msg_wrds;
@@ -358,8 +440,8 @@ char ** snag (char* message, int* size) {
 
   int msg_start = 0;
   int msg_end = 0;
-  while (1) {
-    if ( *(message+msg_end) == ' ' || *(message+msg_end) == '\0') {
+  while (size_limit == 0 || _size < size_limit) {
+    if ( *(message+msg_end) == ' ' || *(message+msg_end) == '\0' || *(message+msg_end) == '\n') {
       // add word to msg_words and increase size.
       if ( _size >= words_size) {
         // realloc more space
@@ -383,6 +465,20 @@ char ** snag (char* message, int* size) {
 
     ++ msg_end;
   }
+
+  msg_wrds = (char**) realloc (msg_wrds, _size * sizeof(char*));
+
+  #ifdef DEBUG
+  for ( int i = 0; i < _size; ++i ) {
+    printf ("Word %d: %s (strlen : %ld) => ", i+1, *(msg_wrds+i), strlen(*(msg_wrds+i)));
+    int j = 0;
+    while ( *(*(msg_wrds+i)+j) != '\0' ) {
+      printf ("[%c:%d] ", *(*(msg_wrds+i)+j),(int) *(*(msg_wrds+i)+j));
+      ++j;
+    }
+    printf ("\n");
+  }
+  #endif
 
   if (size != 0) *size = _size;
   return msg_wrds;
@@ -409,24 +505,31 @@ int isAlnumString (char* str) {
 void logout (int user_index) {
 
   if ( user_index < user_db_size ) {
-    pthread_mutex_lock (&user_db_mutex);
 
     free (*(user_database+user_index));
     if ( user_index + 1 == user_db_index ) {
+
+      #ifdef DEBUG
+      printf ("Case 1\n");
+      #endif
+
       // remove normally, since I was the last to be added to the list
-      *(user_database+user_index) = NULL;
+      *(user_database+user_index) = 0;
       *(user_fds+user_index) = 0;
     }
 
     else { // move the last added user to the spot where this user is logging out...
-    *(user_database+user_index) = *(user_database + user_db_index - 1);
-    *(user_fds + user_index) = *(user_fds + user_db_index - 1);
 
-    *(user_database + user_db_index - 1) = NULL;
-    *(user_fds + user_db_index - 1) = 0;
+      #ifdef DEBUG
+      printf ("Case 2\n");
+      #endif
+      *(user_database+user_index) = *(user_database + user_db_index - 1);
+      *(user_fds + user_index) = *(user_fds + user_db_index - 1);
+
+      *(user_database + user_db_index - 1) = 0;
+      *(user_fds + user_db_index - 1) = 0;
     }
-
-    pthread_mutex_unlock (&user_db_mutex);
+    --user_db_index;
   }
 }
 
@@ -438,4 +541,17 @@ int find(char** lst, int size, char* wrd) {
     if ( strcmp(*(lst+i), wrd) == 0 ) return i;
   }
   return -1;
+}
+
+void who (char** buffer, int* bytes) {
+  strcpy (*buffer, "OK!\n");
+  *bytes = 4;
+
+  for ( int i = 0; i < user_db_index; ++i ) {
+    strcpy (*buffer+*bytes, user_database[i]);
+    *bytes += strlen(user_database[i]);
+    (*buffer)[*bytes] = '\n';
+    ++(*bytes);
+  }
+  buffer[*bytes] = '\0';
 }
